22cac0d889ba470fae82a73f1bbe1d0cd96d752b
kaosker <thorup93@hotmail.com>
2023-02-19 22:11:45 +0100

    changed the splice to use a simpler equal sign

--- max-in-columns.js
+++ max-in-columns.js
@@ -6,7 +6,7 @@ function maxColumn(matrix) {
 
         for (let j = 0; j < currentArr.length; j++){        // iterate inside one of the 3 sub arrays (first one is [ 5,  9, 21] )
             if (currentArr[j] > newArr[j]) {                // if j in the current is bigger than j in the newArr : replace is
-                newArr.splice(j, 1, currentArr[j])          // replace at index j, delete the existing, insert the current j (the num)
+                newArr[.splice(j, 1, currentArr[j]) ]       // replace at index j, delete the existing, insert the current j (the num) /// OR YOU CAN JUST newarr[j] = currentArr[j];
             }
         }
     }
